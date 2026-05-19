694b35f86907e2c3f774b6051b29c46e216322a8
Ming Liu <miliu@google.com>
2026-05-18 23:04:26 -0700

    apply patch from https://paste.googleplex.com/6342340030627840#l=7

--- vllm/model_executor/models/deepseek_v2.py
+++ vllm/model_executor/models/deepseek_v2.py
@@ -1433,6 +1433,9 @@ class DeepseekV2Model(nn.Module):
                 if is_pp_missing_parameter(name, self):
                     continue
 
+                if name not in params_dict:
+                    continue
+
                 param = params_dict[name]
                 weight_loader = param.weight_loader
                 weight_loader(param, loaded_weight, shard_id)
@@ -1504,6 +1507,9 @@ class DeepseekV2Model(nn.Module):
                         if is_pp_missing_parameter(name_mapped, self):
                             continue
 
+                        if name_mapped not in params_dict:
+                            continue
+
                         param = params_dict[name_mapped]
                         # We should ask the weight loader to return success or
                         # not here since otherwise we may skip experts with
@@ -1542,6 +1548,9 @@ class DeepseekV2Model(nn.Module):
                             continue
 
                         if is_pp_missing_parameter(name, self):
+                            continue
+
+                        if name not in params_dict:
                             continue
 
                         param = params_dict[name]
